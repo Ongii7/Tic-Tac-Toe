8040061d2cd603849da123703af11b92f7f81daa
Ongii7 <ongii.k18@gmail.com>
2022-10-05 19:15:43 -0700

    added comments for process of game

---
+++ tictactoe.c
@@ -0,0 +1,21 @@
+//Tic Tac Toe game between 2 players or player vs computer
+
+
+int main() {
+
+    //prompt user for game they wish to play
+
+    //read information from console
+
+    //begin the game and prompt the user to provide their input
+
+    //if two players then ask again for the second player's input
+
+    //function that determines whether they win with their input or not (computer or 2 players)
+
+    //repeat the previous 2 steps until there is a winner or there are no more spots in the rows & cols
+    
+    //if finished then return the result (winner or tie).
+
+    return 0;
+}
