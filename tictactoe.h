40bc1fb24067409dc0b3ba07bb019df012e2d9cf
Ongii7 <ongii.k18@gmail.com>
2022-10-07 21:59:01 -0700

    Fixed error where player would win insufficiently and now able to detect invalid inputs.

--- tictactoe.h
+++ tictactoe.h
@@ -31,22 +31,38 @@ void playerChoice(char board[], char *turn, int *game) {
     if(*turn == 'X'){
         printf("\nPlayer1: make your move: (Choose a number 1-9 for the board #.)");
         scanf("%d", &choice);
+            if(board[choice - 1] != '-') {
+                while(board[choice - 1] != '-') {
+                    printf("\nError! Square already filled. Pick a different square. (1-9):\n");
+                    scanf("%d", &choice);
+                }
+            }
     }
     if(*turn == 'O'){
         //Player chose Simulation vs Player2, second player can add in their inputs 1-9.
         if(*game == 1){
         printf("\nPlayer2: make your move: (Choose a number 1-9 for the board #.)");
         scanf("%d", &choice);
+            if(board[choice - 1] != '-') {
+                while(board[choice - 1] != '-') {
+                    printf("\nError! Square already filled. Pick a different square. (1-9):\n");
+                    scanf("%d", &choice);
+                }
+            }
         }
         //Player chose Simulation vs Computer, use random inputs for computer input 1-9.
         if(*game == 2){
             printf("\nComputer: make your move:\n");
             choice = (rand() % (upper - lower + 1)) + lower;
+            if(board[choice - 1] != '-') {
+                while(board[choice - 1] != '-') {
+                    choice = (rand() % (upper - lower + 1)) + lower;
+                }
+            }
         }
     }
     //if board has an empty spot then player choice is allowed.
-    if(board[choice - 1] == '-')
-        board[choice - 1] = *turn;
+    board[choice - 1] = *turn;
 }
 
 //to check if player won, check horizontal win: (0-2) (3-5) (6-9)
@@ -69,7 +85,7 @@ bool resultCheck(char board[]) {
     }
     //Column check
     if (board[0] != '-') {
-        if (board[0] == board[3] && board[6])
+        if (board[0] == board[3] && board[0] == board[6])
             win = true;;
     }
     if (board[2] != '-') {
